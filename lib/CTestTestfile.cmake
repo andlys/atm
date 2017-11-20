99bf4671ac0149af019090940bf694bb09f03d0f
Andrii Lysenko <senkoily@gmail.com>
2017-11-20 11:28:06 +0200

    remove junk from /lib folder

--- lib/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/denysmelnychenko/Documents/C++/MOOP/atm/atm/lib/fmt-4.0.0
-# Build directory: /Users/denysmelnychenko/Documents/C++/MOOP/atm/atm/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("fmt")
-subdirs("doc")
-subdirs("test")
